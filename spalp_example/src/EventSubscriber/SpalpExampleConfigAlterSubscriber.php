fe1c4e8ea700e51ceaff593823018911af44ed12
Borukar <mangesh.borukar@capgemini.com>
2018-09-10 17:47:11 +0530

    spalp_service : Integrating splap sevices

---
+++ spalp_example/src/EventSubscriber/SpalpExampleConfigAlterSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\spalp_example\EventSubscriber;
+
+use Drupal\spalp\Event\SpalpConfigAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\spalp_example\SpalpExampleInterface;
+
+/**
+ * Class SpalpExampleConfigAlterSubscriber.
+ *
+ * @package Drupal\spalp_example\EventSubscriber
+ */
+class SpalpExampleConfigAlterSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[SpalpConfigAlterEvent::APP_CONFIG_ALTER] = 'doAppConfigAlter';
+
+    return $events;
+  }
+
+  /**
+   * React to app alter event to add additional config.
+   *
+   * @param \Drupal\spalp\Event\SpalpConfigAlterEvent $event
+   *   Spalp App Ids Alter Event.
+   */
+  public function doAppConfigAlter(SpalpConfigAlterEvent $event) {
+    if ($event->getAppId() === SpalpExampleInterface::APP_ID) {
+      $config = $event->getConfig();
+      $additional_config = ['This is for the testing purpose'];
+      $merged_value = array_merge($config, $additional_config);
+      $event->setConfig($merged_value);
+    }
+  }
+
+}
