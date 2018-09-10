128652e57342346add8c4a1df04bf76a3b89d6b5
Malcolm Young <malcolm@rubydesign.co.uk>
2018-09-10 08:14:47 +0100

    Issue #2996051 by malcomio: Import app config and text when installing child modules
    * PHPCS cleanup from previous pull
    
    * Issue #2996135: Move helper methods into a service
    
    * Issue #2992728 by malcomio: Basic JS example module
    
    * Issue #2992728 by malcomio: Basic JS example module
    
    * Issue #2996051 by malcomio: Import app config and text when installing child modules
    
    * Issue #2997110 by malcomio: Use applanding configuration in blocks (#8)

--- spalp_example/js/spalp_example.js
+++ spalp_example/js/spalp_example.js
@@ -1,1 +1,75 @@
-alert('hello');
+/**
+ * Example module for the Single Page Application Landing Page module.
+ *
+ * This example uses jQuery to keep the code simple, but apps can use any
+ * framework (or vanilla JavaScript).
+ */
+
+(function ($, Drupal) {
+
+    Drupal.spalpExample = {};
+
+    /**
+     * Get the language of the current page.
+     *
+     * @return {string}
+     *   The ISO language code of the page.
+     */
+    Drupal.spalpExample.getLanguage = function () {
+        return $('html').attr('lang');
+    };
+
+    /**
+     * Get the config
+     */
+    Drupal.spalpExample.getConfig = function () {
+        // TODO: get this from the node if it's available.
+        const configURL = '/modules/contrib/spalp/spalp_example/spalp_example.config.json'
+        let config = {}
+
+        $.getJSON(
+            configURL, function (data) {
+                config = data;
+
+                const language = Drupal.spalpExample.getLanguage();
+                Drupal.spalpExample.addContent(config, language);
+            }
+        );
+        return config;
+    };
+
+    /**
+     * Add content to the page.
+     *
+     * @param {object} config
+     *   The JSON object with the app configuration.
+     * @param {string} language
+     *   The ISO language code.
+     */
+    Drupal.spalpExample.addContent = function (config, language) {
+        const $appWrapper = $('#spalp_example');
+
+        /*
+         The appText object is used for text that appears within the app.
+         It is keyed by language code.
+         */
+        const heading = '<h2>' + config.appText[language].heading + '</h2>'
+        const body = config.appText[language].body
+
+        $appWrapper.append(heading);
+
+        /*
+          The appConfig object is used for configuration options, such as
+          API keys or endpoint URLs.
+
+          In this simple example, we use it to control the number of times
+          the body is repeated.
+         */
+        for (let i = 0; i < config.appConfig.bodyRepeat; i++) {
+            $appWrapper.append(body);
+        }
+    };
+
+    Drupal.spalpExample.getConfig();
+
+})(jQuery, Drupal)
