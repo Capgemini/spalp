bee9cce30d7f1aebc9243756c9f92274f91062fd
Malcolm Young <malcolm@rubydesign.co.uk>
2018-09-25 17:30:07 +0100

    coding standards fixes

--- src/Event/SpalpConfigLocationAlterEvent.php
+++ src/Event/SpalpConfigLocationAlterEvent.php
@@ -4,6 +4,11 @@ namespace Drupal\spalp\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 
+/**
+ * Class SpalpConfigLocationAlterEvent.
+ *
+ * @package Drupal\spalp\Event
+ */
 class SpalpConfigLocationAlterEvent extends Event {
 
   const CONFIG_LOCATION_ALTER = 'spalp_config_location_alter';
@@ -30,34 +35,46 @@ class SpalpConfigLocationAlterEvent extends Event {
    * @param array $configLocations
    *   Array of file locations, keyed by type.
    */
-  public function __construct($appId, $configLocations) {
+  public function __construct($appId, array $configLocations) {
     $this->appId = $appId;
     $this->configLocations = $configLocations;
   }
 
   /**
-   * @return mixed
+   * Get the app ID for this event.
+   *
+   * @return string
+   *   The app ID.
    */
   public function getAppId() {
     return $this->appId;
   }
 
   /**
-   * @param mixed $appId
+   * Set the app ID for this event.
+   *
+   * @param string $appId
+   *   The app ID.
    */
   public function setAppId($appId) {
     $this->appId = $appId;
   }
 
   /**
-   * @return mixed
+   * Get the paths of the JSON config files.
+   *
+   * @return array
+   *   Array of paths, keyed by type.
    */
   public function getConfigLocations() {
     return $this->configLocations;
   }
 
   /**
+   * Set the paths of the JSON config files.
+   *
    * @param mixed $configLocations
+   *   Array of paths, keyed by type.
    */
   public function setConfigLocations($configLocations) {
     $this->configLocations = $configLocations;
