513d05cad7c2d8ccfa6c1d6dd56bb050dc90d104
Aaron <aaron.ferris@capgemini.com>
2018-11-14 16:19:24 +0000

    Check if node is not empty before attempting to translate it

--- src/Service/Core.php
+++ src/Service/Core.php
@@ -222,21 +222,23 @@ class Core {
     $nid = end($nids);
     $node = $node_storage->load($nid);
 
-    try {
+    if (!empty($node)) {
+      try {
       // Use the translation, if there is one.
-      $node = $node->getTranslation($language);
-    }
-    catch (\InvalidArgumentException $exception) {
-      // If there's no relevant translation, log it.
-      $this->loggerFactory->get('spalp')->notice(
-        $this->t('Attempt to fetch non-existent translation of node @nid to @language for @module module.',
-          [
-            '@nid' => $node->id(),
-            '@language' => $language,
-            '@module' => $module,
-          ]
-        )
-      );
+        $node = $node->getTranslation($language);
+      }
+      catch (\InvalidArgumentException $exception) {
+        // If there's no relevant translation, log it.
+        $this->loggerFactory->get('spalp')->notice(
+          $this->t('Attempt to fetch non-existent translation of node @nid to @language for @module module.',
+            [
+              '@nid' => $node->id(),
+              '@language' => $language,
+              '@module' => $module,
+            ]
+          )
+        );
+      }
     }
 
     return $node;
