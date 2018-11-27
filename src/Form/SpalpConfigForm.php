ca3a6f9d1ac0c4e164e465a60b9a76dc0d3b0ae2
Malcolm Young <malcolm@rubydesign.co.uk>
2018-11-27 07:53:16 +0000

    Issue #3016301: Make JSON forms configurable

---
+++ src/Form/SpalpConfigForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\spalp\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\ConfigFormBase;
+
+
+/**
+ * Provides settings for eu_cookie_compliance module.
+ */
+class SpalpConfigForm extends ConfigFormBase {
+
+  /**
+   * @inheritdoc
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'spalp.settings',
+    ];
+
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getFormId() {
+    return 'spalp_config_form';
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('spalp.settings');
+
+    $form['json_forms_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable JSON Forms'),
+      '#default_value' => $config->get('json_forms_enabled'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('spalp.settings')
+      ->set('json_forms_enabled', $form_state->getValue('json_forms_enabled'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+}
