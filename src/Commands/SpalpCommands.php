476c5593f80faa7857cf478c9b0b9a0e6dd6ad95
Malcolm Young <malcolm@rubydesign.co.uk>
2019-03-14 13:11:07 +0000

    Issue #3040007: Mechanism for exporting current configuration back to JSON
    * Issue #3040007: Mechanism for exporting current configuration back to JSON
    
    * add config diff command
    
    * coding standards
    
    * more meaningful variable names

--- src/Commands/SpalpCommands.php
+++ src/Commands/SpalpCommands.php
@@ -88,4 +88,85 @@ class SpalpCommands extends DrushCommands {
     ]));
   }
 
+  /**
+   * Show the difference between config on the applanding node and in JSON.
+   *
+   * @param string $module
+   *   The machine name of the module.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drush\Exceptions\UserAbortException
+   *
+   * @command spalp:config-diff
+   * @aliases scd
+   */
+  public function configDiff($module = '') {
+    // If we don't have a module from the command, ask for one.
+    if (empty($module)) {
+      $modules = $this->spalpConfig->getAppIds();
+      $module = $this->io()->choice('Which module would you like to import configuration for?', $modules);
+    }
+
+    $json = $this->spalpCore->getConfigFromJson($module);
+    if (empty($json)) {
+      throw new \Exception(dt('@module does not provide JSON configuration.', [
+        '@module' => $module,
+      ]));
+    }
+
+    $node = $this->spalpCore->getAppNode($module);
+    if (empty($node)) {
+      throw new \Exception(dt('There is no app landing node for @module.', [
+        '@module' => $module,
+      ]));
+    }
+
+    $diff = $this->spalpCore->getAppConfigDiff($module);
+    $this->logger()->success(dt('Configuration diff between node @nid and JSON for @module: @diff', [
+      '@nid' => $node->id(),
+      '@module' => $module,
+      '@diff' => print_r($diff, 1),
+    ]));
+
+  }
+
+  /**
+   * Output the current config on the applanding node for a module.
+   *
+   * @param string $module
+   *   The machine name of the module.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drush\Exceptions\UserAbortException
+   *
+   * @command spalp:config-export
+   * @aliases sce
+   */
+  public function configExport($module = '') {
+    // If we don't have a module from the command, ask for one.
+    if (empty($module)) {
+      $modules = $this->spalpConfig->getAppIds();
+      $module = $this->io()->choice('Which module would you like to import configuration for?', $modules);
+    }
+
+    $node = $this->spalpCore->getAppNode($module);
+    if (empty($node)) {
+      throw new \Exception(dt('There is no app landing node for @module.', [
+        '@module' => $module,
+      ]));
+    }
+
+    $config = $this->spalpCore->getAppConfig($module);
+
+    // TODO: output to a file?
+    $this->logger()->success(dt('Current config on node @nid for @module: @config', [
+      '@nid' => $node->id(),
+      '@module' => $module,
+      '@config' => Json::encode($config),
+    ]));
+
+  }
+
 }
--- src/Service/Core.php
+++ src/Service/Core.php
@@ -343,4 +343,80 @@ class Core {
     $node->save();
   }
 
+  /**
+   * Get the difference between config in the JSON file and the applanding node.
+   *
+   * @param string $module
+   *   The machine name of the module.
+   *
+   * @return array
+   *   Associative array of differences.
+   *   'node_only': in the node, but not in JSON for the module.
+   *   'json_only': in JSON, but not in the node.
+   *   'diff': in both arrays, with different values.
+   *   - 'node': the value on the node.
+   *   - 'json': the value in JSON.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getAppConfigDiff($module) {
+    $config_node = $this->getAppConfig($module);
+    $config_json = $this->getConfigFromJson($module);
+
+    $diff = $this->arrayDiffRecursive($config_node, $config_json);
+
+    return array_filter($diff);
+  }
+
+  /**
+   * Recursively compare two arrays.
+   *
+   * @param array $config_node
+   *   The configuration array from the applanding node.
+   * @param array $config_json
+   *   The configuration array from the JSON file.
+   *
+   * @return array
+   *   Associative array of differences.
+   *   'node_only': in $config_node, but not $config_json
+   *   'json_only': in $config_json, but not $config_node
+   *   'diff': in both arrays, with different values.
+   *   - 'node': the value in $config_node
+   *   - 'json': the value in $config_json
+   */
+  public function arrayDiffRecursive(array $config_node, array $config_json) {
+    $result = ['node_only' => [], 'json_only' => [], 'diff' => []];
+    foreach ($config_node as $key => $value) {
+      if (is_array($value) && isset($config_json[$key]) && is_array($config_json[$key])) {
+        $sub_result = $this->arrayDiffRecursive($value, $config_json[$key]);
+        foreach (array_keys($sub_result) as $sub_key) {
+          if (!empty($sub_result[$sub_key])) {
+            $result[$sub_key] = array_merge_recursive($result[$sub_key],
+              [$key => $sub_result[$sub_key]]);
+          }
+        }
+      }
+      else {
+        if (isset($config_json[$key])) {
+          if ($value !== $config_json[$key]) {
+            $result['diff'][$key] = [
+              'node' => $value,
+              'json' => $config_json[$key],
+            ];
+          }
+        }
+        else {
+          $result['node_only'][$key] = $value;
+        }
+      }
+    }
+    foreach ($config_json as $key => $value) {
+      if (!isset($config_node[$key])) {
+        $result['json_only'][$key] = $value;
+      }
+    }
+    return $result;
+  }
+
 }
